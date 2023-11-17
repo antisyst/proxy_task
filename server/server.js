46dcd6d42da5e959132574a6b5c60e079f8a8afd
antisyst <redmiramazan@gmail.com>
2023-11-17 18:49:34 +0400

    first commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get('/posts', async (req, res) => {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
